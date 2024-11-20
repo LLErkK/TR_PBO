ac430aab2b92e5eb45924183c1e4bed5894c8537
Rozaqatg <160761434+Rozaqatg@users.noreply.github.com>
2024-11-20 14:05:12 +0700

    buat database dan koneksi

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,24 @@
 package org.example;
 
+import controller.Database;
+import controller.Koneksi;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Database db = new Database();
+        db.createDatabase();
+
+        Connection conn = Koneksi.koneksi();
+        if (conn != null) {
+            try {
+                conn.close();
+                System.out.println("Koneksi ditutup!");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
